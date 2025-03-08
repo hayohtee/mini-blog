e1636946eab91eb2efdb63d7103b7295685c73d0
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-03-08 23:18:34 +0100

    Add new column to users table

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
     id bigserial PRIMARY KEY,
     username text UNIQUE NOT NULL,
-    password_hash bytea NOT NULL
+    password_hash bytea NOT NULL,
+    created_at timestamp(0) WITH TIME ZONE NOT NULL DEFAULT now(),
 );
