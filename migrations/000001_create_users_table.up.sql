ef90edab23358892f26a1e3252a1d696793dd563
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-03-08 23:19:26 +0100

    Update schema for users

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS users (
     id bigserial PRIMARY KEY,
     username text UNIQUE NOT NULL,
     password_hash bytea NOT NULL,
-    created_at timestamp(0) WITH TIME ZONE NOT NULL DEFAULT now(),
+    created_at timestamp(0) WITH TIME ZONE NOT NULL DEFAULT now()
 );
