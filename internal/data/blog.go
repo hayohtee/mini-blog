e5579535e88779bd45f8b4addf2a7d1ee6a4523b
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-03-08 23:27:54 +0100

    Add Blog struct

---
+++ internal/data/blog.go
@@ -0,0 +1,12 @@
+package data
+
+import "time"
+
+type Blog struct {
+	ID        int64     `json:"id"`
+	UserID    int64     `json:"user_id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	Version   int       `json:"-"`
+}
