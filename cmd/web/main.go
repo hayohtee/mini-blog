a63afb6ea49da1186fc5fc620fadebe641ecfaf0
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-03-08 20:40:21 +0100

    Add main.go

---
+++ cmd/web/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"flag"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Create a JSON logger with minimum level DEBUG.
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	// Load .env file.
+	if err := godotenv.Load(); err != nil {
+		logger.Error("unable to load .env file", slog.String("message", err.Error()))
+		os.Exit(1)
+	}
+
+	// Read the addr and dsn flag from the command-line
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsb", os.Getenv("DATABASE_DSN"), "Database DSN")
+	flag.Parse()
+
+	// Open database connection
+	logger.Info("opening database connection")
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error("error opening database connection", slog.String("message", err.Error()))
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	app := application{
+		logger: logger,
+	}
+
+	srv := http.Server{
+		Addr:         *addr,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  time.Minute,
+		Handler:      app.routes(),
+	}
+
+	slog.Info("starting server", slog.String("addr", srv.Addr))
+	if err := srv.ListenAndServe(); err != nil {
+		logger.Error("error starting up the server", slog.String("message", err.Error()))
+		os.Exit(1)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
